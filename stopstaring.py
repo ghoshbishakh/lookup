08dc53e550d310fb52d49adafbe594c29fc12d5a
ghoshbishakh <ghoshbishakh@gmail.com>
2014-09-25 19:57:02 +0530

    notification in a loop with 15sec interval

--- stopstaring.py
+++ stopstaring.py
@@ -1,1 +1,9 @@
-print "Hello World"
+from gi.repository import Notify
+from time import sleep
+
+Notify.init ("stopstaring")
+Notification=Notify.Notification.new("Stop Staring","take a break MAN!", "")
+while 1:
+	Notification.show ()
+	sleep(15)
+

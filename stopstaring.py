a35259540f9758fdbe11b7a78ae8b855622f7065
ghoshbishakh <ghoshbishakh@gmail.com>
2014-09-25 21:10:42 +0530

    added an icon, and did some clumsy tricks to get home directory

--- stopstaring.py
+++ stopstaring.py
@@ -1,9 +1,11 @@
 from gi.repository import Notify
 from time import sleep
+from os.path import expanduser
+home = expanduser("~")
 
 Notify.init ("stopstaring")
-Notification=Notify.Notification.new("Stop Staring","take a break MAN!", "")
+Notification=Notify.Notification.new("Stop Staring","take a break MAN!", "%s/projects/stopstaring/icon.png"%(home))
 while 1:
 	Notification.show ()
-	sleep(15)
+	sleep(20)
 

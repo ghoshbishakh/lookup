d3eda416230b834ef5fae47c4e13bdf3a759dbef
ghoshbishakh <ghoshbishakh@gmail.com>
2014-09-26 11:43:13 +0530

    improved current directory expand user crap

--- stopstaring.py
+++ stopstaring.py
@@ -2,11 +2,13 @@
 
 from gi.repository import Notify
 from time import sleep
-from os.path import expanduser
-home = expanduser("~")
+import os
+
+currentDirectory = os.getcwd()
+
 
 Notify.init ("stopstaring")
-Notification=Notify.Notification.new("Stop Staring","take a break MAN!", "%s/projects/stopstaring/icon.png"%(home))
+Notification=Notify.Notification.new("Stop Staring","take a break MAN!", "%s/icon.png"%(currentDirectory))
 while 1:
 	Notification.show ()
 	sleep(20)

475310c42cce572dd25409655eebfd80ce68fb99
ghoshbishakh <ghoshbishakh@gmail.com>
2014-09-27 20:22:49 +0530

    just cleaning code

--- stopstaring.py
+++ stopstaring.py
@@ -12,30 +12,8 @@ from subprocess import call
 currentDirectory = os.getcwd()
 
 
-myIndicator = AppIndicator3.Indicator.new(
-    'indicatorId', "%s/icon.png" % (currentDirectory),
-    AppIndicator3.IndicatorCategory.APPLICATION_STATUS)
-
-myIndicator.set_status(AppIndicator3.IndicatorStatus.ACTIVE)
-myIndicator.set_attention_icon("indicator-messages-new")
-
-
 def stopAll(*args):
     Gtk.main_quit()
-
-
-menu = Gtk.Menu()
-menuItem = Gtk.MenuItem("Stop Disturbing Me")
-menu.append(menuItem)
-menuItem.connect("activate", stopAll)
-
-# show the items
-menuItem.show()
-myIndicator.set_menu(menu)
-
-Notify.init("stopstaring")
-Notification = Notify.Notification.new(
-    "Stop Staring", "take a break MAN!", "%s/icon.png" % (currentDirectory))
 
 
 def showNotification(*args):
@@ -43,6 +21,28 @@ def showNotification(*args):
     if sys.platform == 'linux2':
         call(["aplay", "%s/beep.wav" % (currentDirectory)])
     return True
+
+
+menu = Gtk.Menu()
+menuItem = Gtk.MenuItem("Stop Disturbing Me")
+menu.append(menuItem)
+menuItem.connect("activate", stopAll)
+menuItem.show()
+
+
+myIndicator = AppIndicator3.Indicator.new(
+    'indicatorId', "%s/icon.png" % (currentDirectory),
+    AppIndicator3.IndicatorCategory.APPLICATION_STATUS)
+
+myIndicator.set_status(AppIndicator3.IndicatorStatus.ACTIVE)
+myIndicator.set_attention_icon("indicator-messages-new")
+myIndicator.set_menu(menu)
+
+Notify.init("lookup")
+Notification = Notify.Notification.new(
+    "Look Up", "Time to give your eyes a break.", "%s/icon.png" % (currentDirectory))
+
+
 
 GLib.timeout_add_seconds(20, showNotification, None, None)
 

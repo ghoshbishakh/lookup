74019814340db21f563ea527443e73674d78ef93
ghoshbishakh <ghoshbishakh@gmail.com>
2014-09-27 19:34:49 +0530

    Indicator applet added.. everything works.. but the code is a mess

--- stopstaring.py
+++ stopstaring.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+from gi.repository import Gtk
+from gi.repository import AppIndicator3
+from gi.repository import GLib
 from gi.repository import Notify
 from time import sleep
 import os
@@ -8,12 +11,42 @@ from subprocess import call
 
 currentDirectory = os.getcwd()
 
+
+myIndicator = AppIndicator3.Indicator.new(
+    'indicatorId', "%s/icon.png" % (currentDirectory),
+    AppIndicator3.IndicatorCategory.APPLICATION_STATUS)
+
+myIndicator.set_status(AppIndicator3.IndicatorStatus.ACTIVE)
+myIndicator.set_attention_icon("indicator-messages-new")
+
+
+def stopAll(*args):
+    Gtk.main_quit()
+
+
+menu = Gtk.Menu()
+menuItem = Gtk.MenuItem("Stop Disturbing Me")
+menu.append(menuItem)
+menuItem.connect("activate", stopAll)
+
+# show the items
+menuItem.show()
+myIndicator.set_menu(menu)
+
 Notify.init("stopstaring")
 Notification = Notify.Notification.new(
     "Stop Staring", "take a break MAN!", "%s/icon.png" % (currentDirectory))
 
-while 1:
+
+def showNotification(*args):
     Notification.show()
     if sys.platform == 'linux2':
         call(["aplay", "%s/beep.wav" % (currentDirectory)])
-    sleep(20)
+    return True
+
+GLib.timeout_add_seconds(20, showNotification, None, None)
+
+showNotification()
+
+
+Gtk.main()

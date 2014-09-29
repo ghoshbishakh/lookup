1b8f8e44646ce209806ae28b6b07a801e6c11582
ghoshbishakh <ghoshbishakh@gmail.com>
2014-09-29 20:09:59 +0530

    removed notify and setup .. was useless

--- notify.py
+++
@@ -1,8 +0,0 @@
-from gi.repository import Gtk
-from gi.repository import AppIndicator3
-import os
-currentDirectory = os.getcwd()
-
-myIndicator=AppIndicator3.Indicator.new('indicatorId',"%s/icon.png"%(currentDirectory),AppIndicator3.IndicatorCategory.APPLICATION_STATUS)
-myIndicator.set_status(AppIndicator3.IndicatorStatus.ACTIVE)
-Gtk.main()
--- setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup
-
-setup(
-    name='lookup',
-    version='0.1',
-    py_modules=['lookup.py'],
-)
